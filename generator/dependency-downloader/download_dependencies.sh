b7c18801656111f63763bf3b79c75824bb26cd79
Lari Hotari <lari@hotari.net>
2016-09-29 13:45:23 +0300

    Remove obsolete script

--- generator/dependency-downloader/download_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$( dirname "${BASH_SOURCE[0]}" )"
-[ -d ../../mavenRepo ] || mkdir ../../mavenRepo
-MAVEN_REPO_DIR=$( cd ../../mavenRepo && pwd )
-mvn "-Dmaven.repo.local=$MAVEN_REPO_DIR" clean install
